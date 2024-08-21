99d06cb383dcbf261ab72d7e5dbd47b23e8a3cc5
Sabbish99 <programmersabbir99@gmail.com>
2024-08-22 00:38:44 +0600

    last

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,22 @@
 //     const playgroundSection = document.getElementById('play-ground');
 //     playgroundSection.classList.remove('hidden');
 // }
+
+
+
+function continueGame() {
+    //step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+}
+
+
+
+function play() {
+    hideElementById('home-screen')
+    showElementById('play-ground')
+    continueGame();
+}
