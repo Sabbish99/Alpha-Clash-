5821b0a86e772de440867f07696ffb270ef9ca61
Sabbish99 <programmersabbir99@gmail.com>
2024-08-27 01:25:10 +0600

    last

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,26 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed)
+
+
+    // get the expected the pressed
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    console.log(playerPressed, currentAlphabet)
+    // console.log(currentAlphabetElement.innerText);
+
+    if (playerPressed === currentAlphabet) {
+        console.log('you get a point');
+    }
+    else {
+        console.log('you missed a life');
+    }
+
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
 
 
 function continueGame() {
@@ -31,3 +51,5 @@ function play() {
     showElementById('play-ground')
     continueGame();
 }
+
+
